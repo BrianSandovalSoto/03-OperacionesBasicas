78f5c7fc2de4174065a9c676327914579ef4edd6
BrianSandovalSoto <brians_19@hotmail.com>
2016-01-15 15:09:55 -0600

    03-OperacionesBasicas Programa hecho en clase solo con la opción de sumar dos números

---
+++ 03-OperacionesBasicas/src/AppOperaciones.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Brian David Sandoval Soto
+ *
+ */
+public class AppOperaciones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ventana vtn = new Ventana();
+		vtn.setVisible(true);
+	}
+
+}
---
+++ 03-OperacionesBasicas/src/Operaciones.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author Brian David Sandoval Soto
+ *
+ */
+
+
+public class Operaciones {
+	/**
+	 * 
+	 * @param n1 Valor de la primera caja de texto
+	 * @param n2 Valor de la segunda caja de texto
+	 * @return regresa un número entero
+	 */
+	public int sumar(int n1, int n2){
+		return n1+n2;
+	}
+
+}
